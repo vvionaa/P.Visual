1da606594ef4a6164013e865b8bc0a2f95f5ec58
vvionaa <150880085+vvionaa@users.noreply.github.com>
2024-09-02 22:06:00 -0700

    Add files via upload

---
+++ TUGASVIO/src/tugasvio/tugasvio/src/main/java/com/mycompany/tugasvio/AreaKota.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tugasvio;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+class AreaKota {
+
+    static void append(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static void setSelectionEnd(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static void replaceRange(String string, int i, int i0) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ TUGASVIO/src/tugasvio/tugasvio/src/main/java/com/mycompany/tugasvio/Tugasvio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tugasvio;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Tugasvio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
